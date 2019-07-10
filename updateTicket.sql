f9386e4f6ea794145b7bf1f4ab696a75bd2f5c66
IvanRezini <47368365+IvanRezini@users.noreply.github.com>
2019-07-10 19:37:30 -0300

    closes #29

---
+++ updateTicket.sql
@@ -0,0 +1,7 @@
+UPDATE `bdCinema`.`ticket`
+SET
+`usuario_idUsuario` = <{usuario_idUsuario: }>,
+`assento_sala_idAssento` = <{assento_sala_idAssento: }>,
+`reserva` = <{reserva: }>,
+`valor` = <{valor: }>
+WHERE <{where_expression}>;
