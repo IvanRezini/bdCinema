e091f5d9ad38c8fe3983f8ce0218babf6dab51e3
IvanRezini <47368365+IvanRezini@users.noreply.github.com>
2019-07-10 18:56:41 -0300

    closes #11

---
+++ updateAdministrador.sql
@@ -0,0 +1,7 @@
+UPDATE `dbCinema`.`adiministrador`
+SET
+`idAdiministrador` = <{idAdiministrador: }>,
+`nome` = <{nome: }>,
+`usuario` = <{usuario: }>,
+`senha` = <{senha: }>
+WHERE `idAdiministrador` = <{idDo Administrador}>;
