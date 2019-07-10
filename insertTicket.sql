b037fcdcabdb046c4dc8111061d11192bc0dd2fc
IvanRezini <47368365+IvanRezini@users.noreply.github.com>
2019-07-10 19:34:48 -0300

    closes #28

---
+++ insertTicket.sql
@@ -0,0 +1,10 @@
+INSERT INTO `bdCinema`.`ticket`
+(`usuario_idUsuario`,
+`assento_sala_idAssento`,
+`reserva`,
+`valor`)
+VALUES
+(<{usuario_idUsuario: }>,
+<{assento_sala_idAssento: }>,
+<{reserva: }>,
+<{valor: }>);
